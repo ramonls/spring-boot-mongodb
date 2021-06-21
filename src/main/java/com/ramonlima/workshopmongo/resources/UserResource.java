3f8c12a52dfb316b3701da951b83b004f18695d8
Ramon Lima <ramon.lima@outlook.com>
2021-06-21 15:38:36 -0300

    Entity User and Rest

---
+++ src/main/java/com/ramonlima/workshopmongo/resources/UserResource.java
@@ -0,0 +1,26 @@
+package com.ramonlima.workshopmongo.resources;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ramonlima.workshopmongo.domain.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping()
+	public ResponseEntity<List<User>> findAll() {
+		User girafa = new User("1", "Girafina", "girafina@gmail.com");
+		User rato = new User("2", "Ratolino", "ratolino@gmail.com");
+		List<User> list = new ArrayList<>();
+		list.addAll(Arrays.asList(girafa, rato));
+		return ResponseEntity.ok().body(list);
+	}
+}
