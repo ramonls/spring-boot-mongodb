b06f5adb2f4c238191ad780dc40fdf2a8573d538
Ramon Lima <ramon.lima@outlook.com>
2021-06-22 14:01:29 -0300

    Atualizacao de usuario com PUT

--- src/main/java/com/ramonlima/workshopmongo/services/UserService.java
+++ src/main/java/com/ramonlima/workshopmongo/services/UserService.java
@@ -35,6 +35,17 @@ public class UserService {
 		repository.deleteById(id);
 	}
 	
+	public User update(User obj) {
+		Optional<User> newObj = repository.findById(obj.getId());
+		updateData(newObj, obj);
+		return repository.save(newObj.get());
+	}
+	
+	private void updateData(Optional<User> newObj, User obj) {
+		newObj.get().setName(obj.getName());
+		newObj.get().setEmail(obj.getEmail());
+	}
+
 	public User fromDTO(UserDTO objDto) {
 		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
